667adb0a7e5dcf6938754623c4ea9f351971d329
abdalla-amin <abdalla.amin@vodafone.com>
2021-12-12 16:16:15 +0200

    gallary uploaded

--- src/components/Gallary.js
+++ src/components/Gallary.js
@@ -31,11 +31,11 @@ const  Gallary=()=> {
         component="img"
         alt="green iguana"
         height="220"
-        image="https://i.ibb.co/r7MTBp2/12-8-21-46-15.png"
+        image="https://i.ibb.co/wK5DzFQ/1.png"
         />
         <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-            Lizard
+        <a href="https://google.com" target="_blank">Habiba Afifi</a>
         </Typography>
         </CardContent>
         </Card>
@@ -53,17 +53,335 @@ const  Gallary=()=> {
         component="img"
         alt="green iguana"
         height="220"
-        image="https://i.ibb.co/r7MTBp2/12-8-21-46-15.png"
+        image="https://i.ibb.co/44W0JcC/2.png"
         />
         <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-            Lizard 
-        </Typography>
-        <Typography gutterBottom variant="h5" component="div">
-            by amin
+        Habiba Afifi
         </Typography>
         </CardContent>
         </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/C8V9MnX/0010.jpg"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/WsBj37j/Screen-Shot-2021-11-16-at-11-39-44-AM.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            Ziad Hossam
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/wKB1HcY/11-10-21-9-16.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/60yLrhp/12-8-17-50-35.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            Abdalla Amin
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/kSV4c9S/11-10-21-16-34.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/2NdqT6w/milkyway-Andrea.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            andreea
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/MsxRnry/11-10-21-36-9.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/VDtfQRK/11-12-14-21-50.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/n1nCRTd/11-22-20-17-45.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/BrHhcnh/11-22-21-11-46.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/g6bdMZd/11-22-21-36-10.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Hazem Shaltoot
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/5cgfGVt/Screen-Shot-2021-11-08-at-9-38-42-PM.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">ziad hossam
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/r5zYXGB/12-8-17-48-49.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">Abdalla Amin
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
+        <Card sx={{
+            boxShadow: 1,
+            p: 2,
+            minWidth: 200,
+            minHeight: 200,
+            border: 1 ,
+            zIndex: 'tooltip',
+            margin: 2 ,
+            font:15,
+        }}>
+        <CardMedia
+        component="img"
+        alt="green iguana"
+        height="220"
+        image="https://i.ibb.co/CvNb9KL/12-8-17-50-37.png"
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            Abdalla Amin
+        </Typography>
+        </CardContent>
+        </Card>
+        <br></br>
     </div>
     </div>
     );
